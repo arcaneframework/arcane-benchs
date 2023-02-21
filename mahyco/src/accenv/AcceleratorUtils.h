b0c19a0ee1204e53bde58602868ad63cab26721f
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2023-02-21 09:58:09 +0100

    [mahyco] Corrige compilation avec les versions 3.7+ de Arcane.

--- mahyco/src/accenv/AcceleratorUtils.h
+++ mahyco/src/accenv/AcceleratorUtils.h
@@ -237,7 +237,7 @@ class AccMemAdviser {
 class Real3_View8
 {
  public:
-  Real3_View8(NumArray<Real3,2>& v) : m_ptr(v.to1DSpan().data()), nb_cell(v.dim2Size()) {}
+  Real3_View8(NumArray<Real3,MDDim2>& v) : m_ptr(v.to1DSpan().data()), nb_cell(v.dim2Size()) {}
  public:
   ARCCORE_HOST_DEVICE Real3& operator()(int node_index,int cell_index) const
   {
